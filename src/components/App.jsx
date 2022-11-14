01bfdfd865af0d0c2e2565115220f7f344eb5f89
ADMIN <admin@ADMINs-Mac-mini.local>
2022-11-14 13:56:14 -0600

    errors

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,20 @@
 import React, {Fragment} from 'react'
-import './App.css';
+import './index.css';
 import {
   BrowserRouter as Router,
   Routes,
   Route
   
 } from "react-router-dom"; 
+import {
+    Activities,
+    Home,
+    Login,
+    MyRoutines,
+    Navbar,
+    Register,
+    Routines
+} from './'
 
 function App() {
   return (
