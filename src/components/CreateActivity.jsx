22a6ce75fb0df145acbb3bde0fd9392b856b6c67
ADMIN <admin@ADMINs-Mac-mini.local>
2022-11-15 14:25:28 -0600

    create activity done

--- src/components/CreateActivity.jsx
+++ src/components/CreateActivity.jsx
@@ -1,12 +1,40 @@
 import React, {useState} from 'react';
+import { useNavigate } from 'react-router-dom';
+import { createActivity} from '../api-adapter'
 
 const CreateActivity = (props) => {
     const [name, setName] = useState("");
     const [description, setDescription] = useState("")
+    const navigate = useNavigate()
+    async function handleSubmit(event) {
+        event.preventDefault();
+        const newActivity = await createActivity({
+            name,
+            description,
+        }, localStorage.getItem('token'));
+        props.fetchActivities()
+        navigate('/Activities')
+    }
 
 
     return(
-        <div>Create Activity</div>
+        <div>Create Activity
+            <form onSubmit={handleSubmit}>
+                <input
+                placeholder='Name'
+                value={name}
+                onChange={(e) => setName(e.target.value)}
+                label='Activity Name'
+                />
+                <input
+                placeholder='description'
+                value={description}
+                onChange={(e) => setDescription(e.target.value)}
+                label='Activity Description'
+                />
+                <button type='submit'>Submit Activity</button>
+            </form>
+        </div>
     )
 }
 
