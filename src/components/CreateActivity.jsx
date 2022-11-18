645a044edff32bd8d9167c794d474f015716ac6d
ADMIN <admin@ADMINs-Mac-mini.local>
2022-11-18 10:57:49 -0600

    all done with activities

--- src/components/CreateActivity.jsx
+++ src/components/CreateActivity.jsx
@@ -1,49 +1,53 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import { createActivity } from "../api-adapter";
-import { toast } from "react-toastify"
+import { toast } from "react-toastify";
 
 const CreateActivity = (props) => {
+  const [name, setName] = useState("");
+  const [description, setDescription] = useState("");
+  const navigate = useNavigate();
+  async function handleSubmit(event) {
+    event.preventDefault();
+    const newActivity = await createActivity(
+      {
+        name,
+        description,
+      },
+      localStorage.getItem("token")
+    );
+    props.setActivities([newActivity, ...props.activities]);
+    setName("");
+    setDescription("");
+    console.log(newActivity, "new")
+    if (newActivity.name === "NotFound") {
+        toast.error("Activity Already Exists");
+      } else {
+        toast.success("Activity Added");
+      }
+    navigate("/Activities");
+  }
 
-    console.log(props)
-    const [name, setName] = useState("");
-    const [description, setDescription] = useState("")
-    const navigate = useNavigate()
-    async function handleSubmit(event) {
-        event.preventDefault();
-        const newActivity = await createActivity({
-            name,
-            description,
-        }, localStorage.getItem('token'));
-        props.setActivities([newActivity, ...props.activities])
-        setName('')
-        setDescription('')
-
-
-        navigate('/Activities')
-
-    }
-
-
-    return(
-        <div>Create Activity
-            <form onSubmit={handleSubmit}>
-                <input
-                placeholder='Name'
-                value={name}
-                onChange={(e) => setName(e.target.value)}
-                label='Activity Name'
-                />
-                <input
-                placeholder='description'
-                value={description}
-                onChange={(e) => setDescription(e.target.value)}
-                label='Activity Description'
-                />
-                <button type='submit'>Submit Activity</button>
-            </form>
-        </div>
-    )
-}
+  return (
+    <div>
+      Create Activity
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Name"
+          value={name}
+          onChange={(e) => setName(e.target.value)}
+          label="Activity Name"
+        />
+        <input
+          placeholder="description"
+          value={description}
+          onChange={(e) => setDescription(e.target.value)}
+          label="Activity Description"
+        />
+        <button type="submit">Submit Activity</button>
+      </form>
+    </div>
+  );
+};
 
 export default CreateActivity;
